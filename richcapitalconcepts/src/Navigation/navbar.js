9da7102ccd1a7ed42e67461a434e51758ecb164b
Torreythms <torreythms@gmail.com>
2020-06-16 11:56:24 -0400

    minor navigation hover updates

--- richcapitalconcepts/src/Navigation/navbar.js
+++ richcapitalconcepts/src/Navigation/navbar.js
@@ -9,12 +9,12 @@ function Navigation() {
                  <div className="RCC-logo">
                      <h1> RCC</h1>
                  </div>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> Home </a></li>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> About Us     </a></li>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> Events </a></li>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> Community Development</a></li>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> Youth Development</a></li>
-        <li className="nav-item"><a className="nav-item-link b hover-navy" href="#"> Get Involved </a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> Home </a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> About Us     </a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> Events </a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> Community Development</a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> Youth Development</a></li>
+        <li className="nav-item"><a className="nav-item-link b hover-green" href="#"> Get Involved </a></li>
         <li className="nav-item"><a className=" nav-item-button shadow-5 br2" href="#"> Donate </a></li>
       </ul>
     </div>
