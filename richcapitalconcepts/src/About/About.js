460a29a61998e8b2e142b024976f7be6cb673bc1
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-06-16 11:55:41 -0500

    test change

--- richcapitalconcepts/src/About/About.js
+++ richcapitalconcepts/src/About/About.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import './About.css';
 
-
 function About() {
-    return (
-        <div id="about">
-            Hey
-        </div>
-    )
-};
+    return <div id="about">Hey hello old friend the wind</div>;
+}
 
 export default About;
